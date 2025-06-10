d016dd53605c178a78f1d4aba9122254a22c4822
DanAun <23170040+DanAun@users.noreply.github.com>
2025-06-10 17:05:01 +0200

    add: removed magic value

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from scada.scada import app
+from scada.scada import app, SCADA_WEB_PORT
 from ics.ics import start_ics_server
 from malicious.malicious import loop_modbus_request
 
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     ics_thread = Thread(target=start_ics_server, daemon=True)
     ics_thread.start()
     malicious_thread = Thread(target=loop_modbus_request, daemon=True)
-    malicious_thread.start()
+    #malicious_thread.start()
 
     # Run Flask server in main thread (blocking call)
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=SCADA_WEB_PORT)
