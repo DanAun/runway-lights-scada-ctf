22962f891752b77e17ab30465008e83761a90e77
DanAun <23170040+DanAun@users.noreply.github.com>
2025-06-12 14:15:03 +0200

    refactor

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from threading import Thread
+from govee.govee_control import reset_all_strips
 from scada.scada import app, SCADA_WEB_PORT
-from ics.ics import ICS_API_PORT, start_ics_server
+from ics.ics import start_ics_server
 from malicious.malicious import loop_modbus_request
 from waitress import serve
 
 if __name__ == "__main__":
     # Run ICS server in a background thread
-    ics_thread = Thread(target=start_ics_server, daemon=True)
-    ics_thread.start()
-    malicious_thread = Thread(target=loop_modbus_request, daemon=True)
-    malicious_thread.start()
+    ics_thread = Thread(target=start_ics_server, daemon=True).start()
+    malicious_thread = Thread(target=loop_modbus_request, daemon=True).start()
+    Thread(target=reset_all_strips, daemon=True).start()
 
     # Run Flask server in main thread (blocking call)
-    #app.run(host="0.0.0.0", port=SCADA_WEB_PORT)
     serve(app, host='localhost', port=SCADA_WEB_PORT)
