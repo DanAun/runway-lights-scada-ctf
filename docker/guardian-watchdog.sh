cb9525e4ce3bd17b28b46c23067c9064b4c07392
DanAun <23170040+DanAun@users.noreply.github.com>
2025-07-08 15:59:55 +0200

    fix: bug in dockerfile, adding file ownership before copying files

--- docker/guardian-watchdog.sh
+++ docker/guardian-watchdog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    if ! pgrep -x systemd-udevd-watchdog > /dev/null; then
+    if ! pgrep -f systemd-udevd-watchdog > /dev/null; then
         /usr/libexec/coreutils/systemd-udevd-watchdog &
     fi
     sleep 5
